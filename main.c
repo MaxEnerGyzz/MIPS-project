ef259128f9aab1fcffa263821854be39dd6f268f
Mathis Grenier <g-mathis@hotmail.fr>
2021-11-30 09:56:47 +0100

    j'ai testé la traduction avec la structure et ca marche

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "fonction.h"
 
 int main(int argc, char* argv[]){
-  lireInstruction("instr.txt", "result.txt");
-  return 0;
+    char hex[9];
+    remplir_struct();
+    tab_instruction[0].tab_bin[9]='1';
+    tab_instruction[0].tab_bin[10]='1';
+    tab_instruction[0].tab_bin[13]='1';
+    tab_instruction[0].tab_bin[19]='1';
+    binToHex(tab_instruction[0].tab_bin, hex);
+    printf("%s\n", tab_instruction[0].tab_bin);
+    printf("%s\n", hex);
+    
+    return 0;
 }
