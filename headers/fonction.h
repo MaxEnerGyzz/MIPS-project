e76e081c135ccb2d2d7fd3dac6dd515b45e32520
FatMat01 <g-mathis@hotmail.fr>
2022-01-13 16:35:19 +0100

    Update fonction.h
    move struct instr to instructions.h

--- headers/fonction.h
+++ headers/fonction.h
@@ -1,5 +1,5 @@
 #ifndef __MON_H__
-#define __MON_H__ 
+#define __MON_H__
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,38 +10,6 @@
 
 #define NB_INSTRUCTIONS_MIPS 26
 #define NB_INSTRUCTIONS_MAX 500
-
-typedef struct liste_instructions {
-    char *instr; /* NOM de l'instruction */
-    int pos_instr_struct; /* Position de l'instruction dans le tableau de structures d'instruction */
-    int nb_arg; /* Nombre d'arguments de l'instruction */
-    int* arg; /* Arguments de l'instruction */
-    char tab_bin[33]; /* Tableau en binaire associé */
-    char tab_hexa[9]; /* Tableau en héxadécimal associé */
-    int instruction_valide; /* Indique si l'instruction est valide en fonction des arguments registre utilisés...*/
-}liste_instructions;
-   
-
-
-typedef struct liste_instructions_valide {
-    char *instr; /* NOM de l'instruction */
-    int pos_instr_struct; /* Position de l'instruction dans le tableau de structures d'instruction */
-    int nb_arg; /* Nombre d'arguments de l'instruction */
-    int* arg; /* Arguments de l'instruction */
-    char tab_bin[33]; /* Tableau en binaire associé */
-    char tab_hexa[9]; /* Tableau en héxadécimal associé */
-    int instruction_valide; /* Indique si l'instruction est valide en fonction des arguments registre utilisés...*/
-}liste_instructions_valide;
-   
-
-typedef struct instructions {
-    char *instr; /* NOM de l'instruction */
-    int nb_arg; /* Nombre d'arguments */
-    int* pos_arg; /* tableau qui indique le numero des bits de debut et de fin des arguments */
-    char tab_bin[33]; /* Tableau en binaire associé */
-    int *reg; /* Tableau de la taille du nombre d'arguments. Le tab est à 1 si c'est un registre, 0 si c'est un immediate*/
-    int *reg_protege; /* Tableau des arguments où il est possible d'écrire dans un registre: 1 si il est possible d'écrire dans un registre, 0 sinon */
-}instructions; 
 
 void initialiserEmulateur(int mode, char* fichierInstr, char* fichierResult, int nb_instructions_entree, registre* tab_registre, instructions* tab_instruction, liste_instructions* tab_liste_instructions, liste_instructions* tab_liste_instructions_val); /* Initialise le simulateur
 en initialisant les valeurs des structures d'instructions et de registre. Execute egalement lireInstruction() qui permet d'enregistrer le fichier d'entree dans une structure. */
@@ -67,4 +35,3 @@ int compte_nb_instr_val(int nb_instr, liste_instructions* tab_liste_instructions
 int choix_mode(char* argv_1, char* argv_2, int arg_c); /* Renvoie 1 si le mode interactif sansest selectionne, 0 si c'est le mode non interactif*/
 
 #endif
-
